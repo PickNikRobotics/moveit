89150e22a5a69961460de2264be7e067f130ded6
JafarAbdi <cafer.abdi@gmail.com>
2019-09-16 20:17:36 +0300

    Update moveit_cpp_test.cpp

--- moveit_ros/planning_interface/test/moveit_cpp_test.cpp
+++ moveit_ros/planning_interface/test/moveit_cpp_test.cpp
@@ -6,6 +6,7 @@
 
 // Main class
 #include <moveit/moveit_cpp/moveit_cpp.h>
+#include <moveit/moveit_cpp/planning_component.h>
 
 namespace moveit
 {
@@ -16,23 +17,32 @@ class MoveItCppTest : public ::testing::Test
 public:
   void SetUp() override
   {
-    nh_ptr = std::make_unique<ros::NodeHandle>("~");
-    moveit_cpp_ptr = std::make_shared<MoveitCpp>(*nh_ptr);
-  }
+    nh_ = ros::NodeHandle("~");
+    moveit_cpp_ptr = std::make_shared<MoveitCpp>(nh_);
+    // Initialize scene and planning components
+    //    moveit_cpp_ptr->getPlanningSceneMonitor()->startWorldGeometryMonitor();
+    //    moveit_cpp_ptr->getPlanningSceneMonitor()->setPlanningScenePublishingFrequency(500);
+    //    moveit_cpp_ptr->getPlanningSceneMonitor()->setStateUpdateFrequency(500);
 
-  void TearDown() override
-  {
+    planning_component_ptr = std::make_shared<PlanningComponent>("manipulator", moveit_cpp_ptr);
   }
 
 protected:
-  std::unique_ptr<ros::NodeHandle> nh_ptr;
+  ros::NodeHandle nh_;
   MoveitCppPtr moveit_cpp_ptr;
+  PlanningComponentPtr planning_component_ptr;
 };
 
-// TODO(JafarAbdi) delete
-TEST_F(MoveItCppTest, DummyTest)
+TEST_F(MoveItCppTest, GetCurrentStateTest)
 {
-  ASSERT_EQ(1, 1);
+  auto robot_model = moveit_cpp_ptr->getRobotModel();
+  auto robot_state = std::make_shared<robot_state::RobotState>(robot_model);
+  EXPECT_TRUE(moveit_cpp_ptr->getCurrentState(robot_state, 0.0));
+}
+
+TEST_F(MoveItCppTest, NameOfPlanningGroupTest)
+{
+  EXPECT_STREQ(planning_component_ptr->getName().c_str(), "manipulator");
 }
 }  // namespace planning_interface
 }  // namespace moveit
@@ -40,7 +50,7 @@ TEST_F(MoveItCppTest, DummyTest)
 int main(int argc, char** argv)
 {
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "MoveItCpp_test");
+  ros::init(argc, argv, "moveit_cpp_test");
 
   ros::AsyncSpinner spinner(1);
   spinner.start();
