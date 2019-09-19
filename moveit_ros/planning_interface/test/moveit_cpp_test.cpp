e11eca0c050c8cb3e91710b21b6b2056445cc888
JafarAbdi <cafer.abdi@gmail.com>
2019-09-19 13:41:33 +0300

    moveit_cpp_test.cpp: add description for each test, Clean the tests

--- moveit_ros/planning_interface/test/moveit_cpp_test.cpp
+++ moveit_ros/planning_interface/test/moveit_cpp_test.cpp
@@ -54,39 +54,42 @@ protected:
   geometry_msgs::Pose start_pose;
 };
 
+// Test the current and the initial state of the Panda robot
 TEST_F(MoveItCppTest, GetCurrentStateTest)
 {
   ros::Duration(1.0).sleep();  // Otherwise joint_states will result in an invalid robot state
   auto robot_model = moveit_cpp_ptr->getRobotModel();
   auto robot_state = std::make_shared<robot_state::RobotState>(robot_model);
   EXPECT_TRUE(moveit_cpp_ptr->getCurrentState(robot_state, 0.0));
-  //  std::vector<double> joints_vals;
-  //  robot_state->copyJointGroupPositions(PLANNING_GROUP, joints_vals);
-  //  EXPECT_NEAR(joints_vals[0], 0.0, 0.001);     // panda_joint1
-  //  EXPECT_NEAR(joints_vals[1], -0.785, 0.001);  // panda_joint2
-  //  EXPECT_NEAR(joints_vals[2], 0.0, 0.001);     // panda_joint3
-  //  EXPECT_NEAR(joints_vals[3], -2.356, 0.001);  // panda_joint4
-  //  EXPECT_NEAR(joints_vals[4], 0.0, 0.001);     // panda_joint5
-  //  EXPECT_NEAR(joints_vals[5], 1.571, 0.001);   // panda_joint6
-  //  EXPECT_NEAR(joints_vals[6], 0.785, 0.001);   // panda_joint7
+  // Make sure the Panda robot is in "ready" state which is loaded from fake_controller.yaml
+  std::vector<double> joints_vals;
+  robot_state->copyJointGroupPositions(PLANNING_GROUP, joints_vals);
+  EXPECT_NEAR(joints_vals[0], 0.0, 0.001);     // panda_joint1
+  EXPECT_NEAR(joints_vals[1], -0.785, 0.001);  // panda_joint2
+  EXPECT_NEAR(joints_vals[2], 0.0, 0.001);     // panda_joint3
+  EXPECT_NEAR(joints_vals[3], -2.356, 0.001);  // panda_joint4
+  EXPECT_NEAR(joints_vals[4], 0.0, 0.001);     // panda_joint5
+  EXPECT_NEAR(joints_vals[5], 1.571, 0.001);   // panda_joint6
+  EXPECT_NEAR(joints_vals[6], 0.785, 0.001);   // panda_joint7
 }
 
-// TODO(JafarAbdi) unnecessary
+// Test the name of the planning group used by PlanningComponent for the Panda robot
 TEST_F(MoveItCppTest, NameOfPlanningGroupTest)
 {
   EXPECT_STREQ(planning_component_ptr->getName().c_str(), "panda_arm");
 }
 
+// Test setting the start state of the plan to the current state of the robot
 TEST_F(MoveItCppTest, TestSetStartStateToCurrentState)
 {
   planning_component_ptr->setStartStateToCurrentState();
-  // TODO(JafarAbdi) shouldn't be here v
   planning_component_ptr->setGoal(target_pose1, "panda_link8");
 
   ASSERT_TRUE(planning_component_ptr->plan());
   // TODO(JafarAbdi) adding testing to the soln state
 }
 
+// Test setting the goal using geometry_msgs::PoseStamped and a robot's link name
 TEST_F(MoveItCppTest, TestSetGoalFromPoseStamped)
 {
   planning_component_ptr->setStartStateToCurrentState();
@@ -97,6 +100,7 @@ TEST_F(MoveItCppTest, TestSetGoalFromPoseStamped)
   ASSERT_TRUE(planning_component_ptr->plan());
 }
 
+// Test setting the plan start state using robot_state::RobotState
 TEST_F(MoveItCppTest, TestSetStartStateFromRobotState)
 {
   auto start_state = *(moveit_cpp_ptr->getCurrentState());
@@ -108,6 +112,7 @@ TEST_F(MoveItCppTest, TestSetStartStateFromRobotState)
   ASSERT_TRUE(planning_component_ptr->plan());
 }
 
+// Test settting the goal of the plan using a robot_state::RobotState
 TEST_F(MoveItCppTest, TestSetGoalFromRobotState)
 {
   auto target_state = *(moveit_cpp_ptr->getCurrentState());
@@ -131,7 +136,5 @@ int main(int argc, char** argv)
 
   int result = RUN_ALL_TESTS();
 
-  spinner.stop();
-  ros::shutdown();
   return result;
 }
